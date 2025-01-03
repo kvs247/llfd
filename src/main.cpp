f2ac8822590dc18ad6ef9f606e5fddfd919aad74
Kyle <kylesch115@gmail.com>
2025-01-02 22:43:28 -0700

    remove QR/Eigen code (replaced by kmath)

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <kmath/Matrix/Eigen.hpp>
 #include <kmath/Matrix/Matrix.hpp>
 
 #include "QRAlgo/QRAlgo.hpp"
@@ -9,31 +10,21 @@ int main()
   // Matrix m({{12, -51, 4}, {6, 167, -68}, {-4, 24, -41}});
   // Matrix m({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
   // Matrix m({{2, -2, 18}, {2, 1, 0}, {1, 2, 0}});
+  // Matrix m({
+  //     {2, 0, -1, -1, 0},
+  //     {0, 0, 0, 0, 0},
+  //     {-1, 0, 1, 0, 0},
+  //     {-1, 0, 0, 1, 0},
+  //     {0, 0, 0, 0, 0},
+  // });
+
   Matrix m({
-      {2, 0, -1, -1, 0},
-      {0, 0, 0, 0, 0},
-      {-1, 0, 1, 0, 0},
-      {-1, 0, 0, 1, 0},
-      {0, 0, 0, 0, 0},
+      {1, -1, 0},
+      {-1, 2, -1},
+      {0, -1, 1},
   });
 
-  const auto mQRDecomp = QR_GS::qrDecomp(m);
-  const auto q = mQRDecomp.first;
-  const auto r = mQRDecomp.second;
-  const auto qr = q * r;
-
-  std::cout << "main:\n";
-  std::cout << "M:\n";
-  m.print(std::cout);
-  std::cout << "Q:\n";
-  q.print(std::cout);
-  std::cout << "R:\n";
-  r.print(std::cout);
-  std::cout << "Q * R:\n";
-  qr.print(std::cout);
-
-  std::cout << "\nQR Algo:\n";
-  const auto res = QR_Algo::qrAlgo(m);
+  const auto res = Eigen::compute(m);
 
   for (const auto &x : res)
   {
